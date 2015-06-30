ae71459fc76dcd86611ce2008a5abc6d95b69163
Nuno Costa <nunofilipe.costa@gmail.com>
2015-07-01 00:26:58 +0200

    bulk checkin

---
+++ PlaygroundKnockout/src/PlaygroundKnockout.Web.Latests/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿namespace PlaygroundKnockout.Web.Latests.Controllers
+{
+    using Microsoft.AspNet.Mvc;
+
+    /// <summary>
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// </summary>
+        /// <returns>
+        /// </returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <returns>
+        /// </returns>
+        public IActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <returns>
+        /// </returns>
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <returns>
+        /// </returns>
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
